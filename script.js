bf71e2c72106a8903a5ba60888cbccb8c606c945
Amir Hossein Jobeiri <jobeiri.programmer@gmail.com>
2020-12-16 12:42:09 -0800

    13. Working With Classes

--- script.js
+++ script.js
@@ -7,6 +7,19 @@ const btnOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnOpenModal);
 
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnOpenModal.length; i++) {
-  console.log(btnOpenModal[i].textContent);
+  btnOpenModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
